f748b66b86030bcf9f7bab8d45bc6050cf8bf489
martin <martin@comets.kr>
2022-09-11 04:06:12 +0900

    fix: modify to deploy on heroku

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ mongodb.connect(
 
       const app = require("./app");
       const server = http.createServer(app);
-      let PORT = 3000;
+      let PORT = process.env.PORT || 3000;
       server.listen(PORT, function () {
         console.log(
           `The server is running successfully on port: ${PORT}, http://localhost:${PORT}`
