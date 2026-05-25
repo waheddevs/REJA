632f6851362ed3bf5f2771b9d811c8afdf3dca13
waheddevs <abduvohid_gulomov@mail.ru>
2026-05-25 19:29:52 +0900

    fix: fixed server error

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ mongodb.connect(
 
       const app = require("./app");
       const server = http.createServer(app);
-      let PORT = process.env.PORT || 3000;
+      let PORT = process.env.PORT || 3009;
       server.listen(PORT, function () {
         console.log(
           `The server is running successfully on http://localhost:${PORT}`,
